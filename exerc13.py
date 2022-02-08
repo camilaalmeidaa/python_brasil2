469cf4cbcdc9c612a98e1c46ca1efbf8ff53c233
camilaalmeidaa <99203530+camilaalmeidaa@users.noreply.github.com>
2022-02-08 16:54:29 -0300

    Add files via upload

---
+++ exerc13.py
@@ -0,0 +1,39 @@
+def dia():
+    
+    opcao = int(input("*Exercicio13* Digite um numero entre 1 e 7 e será mostrado o dia da semana correspondente: "))
+    
+    if(opcao == 1):
+        
+        print("1 - Domingo\n")
+    
+    elif(opcao == 2):
+        
+        print("2 - Segunda\n")
+    
+    elif(opcao == 3):
+        
+        print("3 - Terça\n")
+    
+    elif(opcao == 4):
+        
+        print("4 - Quarta\n")
+    
+    elif(opcao == 5):
+        
+        print("5 - Quinta\n")
+    
+    elif(opcao == 6):
+        
+        print("6 - Sexta\n")
+    
+    elif(opcao == 7):
+        
+        print("7 - Sábado\n")
+    
+    else:
+        
+        print("Valor inválido\n")
+    
+    
+    
+    
