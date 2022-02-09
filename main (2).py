5ad81fad5bfefe08b3ba9f1a715ba5cda14e2d0c
camilaalmeidaa <99203530+camilaalmeidaa@users.noreply.github.com>
2022-02-09 13:20:21 -0300

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,57 @@
+import exerc1
+import exerc2
+import exerc3
+import exerc4
+import exerc5
+import exerc6
+import exerc7
+import exerc8
+import exerc9
+import exerc10
+import exerc11
+import exerc12
+import exerc13
+import exerc14
+import exerc15
+import exerc16
+import exerc17
+import exerc18
+import exerc19
+import exerc20
+import exerc21
+import exerc22
+import exerc23
+import exerc24
+import exerc25
+import exerc26
+import exerc27
+import exerc28
+
+exerc1.maior()
+exerc2.valor()
+exerc3.sexo()
+exerc4.letra()
+exerc5.media()
+exerc6.compara()
+exerc7.compara2()
+exerc8.preco()
+exerc9.decrescente()
+exerc10.turno()
+exerc11.reajuste()
+exerc12.salario()
+exerc13.dia()
+exerc14.conceito()
+exerc15.triangulo()
+exerc16.equacao()
+exerc17.bissexto()
+exerc18.checa_data()
+exerc19.num()
+exerc20.aprova()
+exerc21.caixa()
+exerc22.par_impar()
+exerc23.int_decimal()
+exerc24.escolha_operacao()
+exerc25.perguntas()
+exerc26.posto()
+exerc27.frutas()
+exerc28.carne()
